61b3f1b68ca938203f43410e05b59072edcf6910
PunyanshSingla <punyanshcoder@gmail.com>
2026-06-09 20:23:49 +0530

    feat: initialize Express server project with configuration and dependency tracking

---
+++ node.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World To Devops , Taking Devops Taste")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`)
+})
