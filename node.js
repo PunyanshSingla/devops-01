2bc04eec38a8b9380f57bb78458fd4bc83555f02
PunyanshSingla <punyanshcoder@gmail.com>
2026-06-09 20:57:29 +0530

    feat: update welcome message in response body to indicate CI/CD pipeline deployment

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello World To Devops , Taking Devops Taste , Deployed successfully for first time in life")
+    res.send("Hello World To Devops , Taking Devops Taste , Deployed successfully for first time in life, updated by ci cd pipeline i am too exicted")
 })
 
 app.listen(process.env.PORT || 3000, () => {
